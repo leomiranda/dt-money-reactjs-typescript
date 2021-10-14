fef8d852a61631b970c0138d30e3a1bbeeec0058
Leo Miranda <contato@leomiranda.com>
2021-10-14 17:46:56 -0400

    feat: create global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from './assets/styles/global';
+
 export function App() {
 	return (
 		<div className='App'>
 			<h1>Hello DT Money</h1>
+			<GlobalStyle />
 		</div>
 	);
 }
